2a9cb91424a70181c7ed12923ab07c92471d4dbd
HODOR72 <72arkhipov@mail.ru>
2023-07-02 20:52:19 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
-    origin: 'https://www.next-payment.site',
+    origin: 'http://www.next-payment.site',
     methods: 'GET,PUT,POST,DELETE',
     credentials: true,
   });
