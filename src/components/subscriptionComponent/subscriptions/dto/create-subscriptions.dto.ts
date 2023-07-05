b52705e0eccc1d943d3746ecba6bcf7527b8823a
HODOR72 <72arkhipov@mail.ru>
2023-07-05 12:40:44 +0500

    fix

--- src/components/subscriptionComponent/subscriptions/dto/create-subscriptions.dto.ts
+++ src/components/subscriptionComponent/subscriptions/dto/create-subscriptions.dto.ts
@@ -1,21 +1,34 @@
-import { IsEmail, IsNumber, IsPositive, IsString, Length } from 'class-validator'
+import {
+  IsEmail,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Length,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateSubscriptionsDto {
   @IsString({ message: 'The value must be a string' })
   @IsEmail({}, { message: 'Incorrect Email' })
-  @Length(6, 128, { message: 'The userEmail must be at least 6 and no more than 128 characters' })
-  readonly userEmail: string
+  @Length(6, 128, {
+    message: 'The userEmail must be at least 6 and no more than 128 characters',
+  })
+  readonly userEmail: string;
 
   @IsNumber({}, { message: 'The value must be a number' })
   @IsPositive({ message: 'The value must be a positive number' })
-  readonly subscriptions_period_id: number
+  readonly subscriptions_period_id: number;
 
   @IsString({ message: 'The value must be a string' })
-  readonly payment_amount: string
+  readonly payment_amount: string;
 
   @IsString({ message: 'The value must be a string' })
-  readonly start_of: string
+  readonly start_of: string;
 
   @IsString({ message: 'The value must be a string' })
-  readonly end_of: string
+  readonly end_of: string;
+
+  @IsOptional()
+  @IsString({ message: 'The value must be a string' })
+  readonly subscriptions_id?: string;
 }
--- src/components/subscriptionComponent/subscriptions/models/subscriptions.model.ts
+++ src/components/subscriptionComponent/subscriptions/models/subscriptions.model.ts
@@ -1,40 +1,47 @@
-import { Column, DataType, ForeignKey, Table } from 'sequelize-typescript'
-import { EntityModel } from '../../../../classes/core/entity.model'
-import { Users } from '../../../usersComponent/users/models/users.model'
-import { SubscriptionPeriods } from '../../subscription-periods/models/subscription-periods.model'
+import { Column, DataType, ForeignKey, Table } from 'sequelize-typescript';
+import { EntityModel } from '../../../../classes/core/entity.model';
+import { Users } from '../../../usersComponent/users/models/users.model';
+import { SubscriptionPeriods } from '../../subscription-periods/models/subscription-periods.model';
 
 interface SubscriptionsCreationAttrs {
-  user_id: string
-  subscription_period_id: number
-  payment_amount: string
-  start_of: string
-  end_of: string
+  user_id: string;
+  subscription_period_id: number;
+  payment_amount: string;
+  start_of: string;
+  end_of: string;
+  subscription_id?: string;
 }
 
 @Table({ tableName: 'Subscriptions' })
-export class Subscriptions extends EntityModel<Subscriptions, SubscriptionsCreationAttrs> {
+export class Subscriptions extends EntityModel<
+  Subscriptions,
+  SubscriptionsCreationAttrs
+> {
   @Column({
     type: DataType.INTEGER,
     unique: true,
     autoIncrement: true,
     primaryKey: true,
   })
-  id: number
+  id: number;
 
   @ForeignKey(() => Users)
   @Column({ type: DataType.STRING, allowNull: false })
-  user_id: string
+  user_id: string;
 
   @ForeignKey(() => SubscriptionPeriods)
   @Column({ type: DataType.INTEGER, allowNull: false })
-  subscription_period_id: number
+  subscription_period_id: number;
 
   @Column({ type: DataType.STRING, allowNull: false })
-  payment_amount: string
+  payment_amount: string;
 
   @Column({ type: DataType.DATE, allowNull: false })
-  start_of: string
+  start_of: string;
 
   @Column({ type: DataType.DATE, allowNull: false })
-  end_of: string
+  end_of: string;
+  
+  @Column({ type: DataType.STRING, allowNull: true }) // Добавлен столбец для subscription_id
+  subscription_id: string;
 }
