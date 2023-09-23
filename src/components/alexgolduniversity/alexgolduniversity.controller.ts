e6663d7de554971c16a59bffd12f7037bd030727
DanIv <daniv@bwg-io.site>
2023-09-23 23:42:07 +0300

    fix

--- src/components/alexgolduniversity/alexgolduniversity.controller.ts
+++ src/components/alexgolduniversity/alexgolduniversity.controller.ts
@@ -9,10 +9,6 @@ export class AlexgolduniversityController {
   @Redirect('https://alexgolduniversity.com/success-page', 302)
   async create(@Body() dto) {
     console.log(dto)
-    const LeadDyno = require('https://static.leaddyno.com/js')
-    LeadDyno.key = "1b12ac3d67141a7e49e12ca7aa37a96c54158519";
-    await LeadDyno.recordPurchase(dto.sender_email, {
-      purchase_amount: '49'
-    });
+    return { url: `https://alexgolduniversity.com/success-page?email=${dto.sender_email}` };
   }
 }
